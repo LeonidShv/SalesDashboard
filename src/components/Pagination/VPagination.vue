050101a7738c7bbb7d7a9929d2ff08b10c7cf765
LeonidShv <matembit1996@gmail.com>
2024-10-19 12:33:44 +0300

    infra: setup UI base components

---
+++ src/components/Pagination/VPagination.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-pagination
+    :current-page="currentPage"
+    background
+    layout="prev, pager, next"
+    :total="amount"
+    :page-size="10"
+    @update:current-page="update"
+    hide-on-single-page
+  />
+</template>
+
+<script setup>
+import { ElPagination } from 'element-plus'
+import 'element-plus/es/components/pagination/style/css'
+// TODO: amount need to get from the server, need for pagination
+const amount = 1000
+const emit = defineEmits(['update:v-model'])
+
+function update(e) {
+  emit('update', e)
+}
+defineProps({
+  currentPage: {
+    type: Number,
+    default: 1,
+  },
+  configuration: {
+    type: Array,
+    default: () => [],
+  },
+})
+</script>
---
+++ src/components/Switch/VSwitch.vue
@@ -0,0 +1,30 @@
+<template>
+  <ElSwitch
+    :value="value"
+    @change="value => $emit('update:v-model', value)"
+    :active-action-icon="activeIcon"
+    :inactive-action-icon="inActiveIcon"
+  />
+</template>
+
+<script setup>
+import { ElSwitch } from 'element-plus'
+import 'element-plus/es/components/switch/style/css'
+
+defineEmits(['update:v-model'])
+
+defineProps({
+  activeIcon: {
+    type: Object,
+    default: () => {},
+  },
+  inActiveIcon: {
+    type: Object,
+    default: () => {},
+  },
+  value: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
